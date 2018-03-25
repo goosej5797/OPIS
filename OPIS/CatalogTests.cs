1b6ac724cd4b9d6f04769dbd6d54037ba791e7f0
House, Becky <bckhouse23@gmail.com>
2018-03-25 16:45:35 -0400

    added catalog tests

---
+++ OPIS/CatalogTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace OPIS
+{
+    [TestFixture]
+    class CatalogTests
+    {
+        [Test]
+        public void TestCatalog_BuildCatalog()
+        {
+
+        }
+    }
+}
